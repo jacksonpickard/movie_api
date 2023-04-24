ea2af37dc5cb165dcb666776aef2ec7c641a85b0
jacksonpickard <jackpickard2@gmail.com>
2023-04-24 10:53:53 -0600

    added error: message

--- auth.js
+++ auth.js
@@ -17,12 +17,13 @@ let generateJWTToken = (user) => {
 /* POST login. */
 module.exports = (router) => {
     router.post("/login", (req, res) => {
+
         passport.authenticate("local", { session: false }, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: "Something is not right",
                     user: user,
-                    error,
+                    error: error.message,
                 });
             }
             req.login(user, { session: false }, (error) => {
